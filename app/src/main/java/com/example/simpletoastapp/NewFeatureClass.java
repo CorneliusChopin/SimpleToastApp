5db6ea3a544cb36433b9480b8a06b65ff5509015
Cornelius Chopin <cornelius@richmediacs.com>
2016-05-14 22:58:45 -0500

    Version2 code added

---
+++ app/src/main/java/com/example/simpletoastapp/NewFeatureClass.java
@@ -0,0 +1,11 @@
+package com.example.simpletoastapp;
+
+/**
+ * Created by Cornelius on 5/14/2016.
+ */
+public class NewFeatureClass {
+    public NewFeatureClass() {
+        //Code goes here...
+    }
+
+}
